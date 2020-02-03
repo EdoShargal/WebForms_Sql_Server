283b3413eb8b27b40079cbfa756454c13b928e28
edo shargal <edosh528@gmail.com>
2020-02-03 23:35:03 +0200

    first commit

---
+++ WebForms/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForms.Default" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>  
+    <form id="form1" runat="server">  
+        <div>  
+            <h3>Search form </h3>
+             <div>  
+                <table class="auto-style1">  
+                    <tr>  
+                        <td>Company name:</td>
+                        <td><asp:TextBox ID="CompanyName" runat="server" /></td>
+                    </tr>
+                    <asp:Label ID="Label1" runat="server"></asp:Label>
+                    <tr>  
+                        <td>Search:</td>
+                        <td>  
+                        <asp:RadioButtonList ID="SearchType" runat="server">  
+                            <asp:ListItem>Equal</asp:ListItem>  
+                            <asp:ListItem>StartWith</asp:ListItem>  
+                            <asp:ListItem>EndWith</asp:ListItem>  
+                            <asp:ListItem>Middle</asp:ListItem>  
+                        </asp:RadioButtonList>  
+                    </td>  
+                    </tr>
+                </table>
+            </div>
+            <asp:Button ID="Button1" Text="Submit" runat="server" OnClick="Button1_Click"/>
+        </div>  
+        <asp:DataGrid ID="DataGrid1" runat="server">  
+        </asp:DataGrid> 
+        
+    </form>  
+     
+        
+</body>  
+                      
+
+</html>
---
+++ WebForms/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string CONNECTION_STRING = "data source=edo-pc\\sqlexpress; database=Northwind; integrated security=SSPI";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataGrid1.DataSource = null;
+            DataGrid1.DataBind();
+            Label1.Text = "";
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                string sqlQuery = "";
+                if (String.IsNullOrEmpty(CompanyName.Text))
+                {
+                    sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
+                            "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
+                            "group by C.CustomerID, C.CompanyName";
+                }
+                else
+                {
+                    string howToSearch = SearchType.SelectedValue;
+                    string valueToSearc = CompanyName.Text;
+                    switch (howToSearch)
+                    {
+                        case "Equal":
+                            sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
+                            "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
+                            "where C.CompanyName = " + "'" + valueToSearc + "' " +
+                            "group by C.CustomerID, C.CompanyName";
+                            break;
+                        case "StartWith":
+                            sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
+                            "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
+                            "where C.CompanyName like " + "'" + valueToSearc + "%' " +
+                            "group by C.CustomerID, C.CompanyName";
+                            break;
+                        case "EndWith":
+                            sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
+                            "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
+                            "where C.CompanyName like " + "'%" + valueToSearc + "' " +
+                            "group by C.CustomerID, C.CompanyName";
+                            break;
+                        case "Middle":
+                            sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
+                            "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
+                            "where C.CompanyName like " + "'%" + valueToSearc + "%' " +
+                            "group by C.CustomerID, C.CompanyName";
+                            break;
+                        default:
+                            Label1.Text = "You didnt pick how to search";
+                            return;
+
+                    }
+
+
+
+                }
+                SqlDataAdapter sde = new SqlDataAdapter(sqlQuery, con);
+                DataSet ds = new DataSet();
+                sde.Fill(ds);
+                DataGrid1.DataSource = ds;
+                DataGrid1.DataBind();
+            }
+        }
+    }
+}
