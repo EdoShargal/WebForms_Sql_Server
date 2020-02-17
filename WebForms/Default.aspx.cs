dc925f6fa95b50d58ddcaabf81e792c385493076
edo shargal <edosh528@gmail.com>
2020-02-17 18:39:43 +0200

    Adding params against sql injection

--- WebForms/Default.aspx.cs
+++ WebForms/Default.aspx.cs
@@ -6,7 +6,10 @@ namespace WebForms
 {
     public partial class Default : System.Web.UI.Page
     {
-        string CONNECTION_STRING = "data source=e<server>; database=Northwind; integrated security=SSPI";
+
+
+        //TODO: put the connection in Web.config
+        string CONNECTION_STRING = "data source=<server>; database=Northwind; integrated security=SSPI";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -15,9 +18,13 @@ namespace WebForms
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            //erase the current table before search
             DataGrid1.DataSource = null;
             DataGrid1.DataBind();
-            Label1.Text = "";
+
+
+            string valueToSearch = CompanyName.Text;
+
             using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
             {
                 string sqlQuery = "";
@@ -30,31 +37,31 @@ namespace WebForms
                 else
                 {
                     string howToSearch = SearchType.SelectedValue;
-                    string valueToSearc = CompanyName.Text;
+                    
                     switch (howToSearch)
                     {
                         case "Equal":
                             sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
                             "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
-                            "where C.CompanyName = " + "'" + valueToSearc + "' " +
+                            "where C.CompanyName = @valueToSearch " +
                             "group by C.CustomerID, C.CompanyName";
                             break;
                         case "StartWith":
                             sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
                             "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
-                            "where C.CompanyName like " + "'" + valueToSearc + "%' " +
+                            "where C.CompanyName like @valueToSearch + '%' "+
                             "group by C.CustomerID, C.CompanyName";
                             break;
                         case "EndWith":
                             sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
                             "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
-                            "where C.CompanyName like " + "'%" + valueToSearc + "' " +
+                            "where C.CompanyName like '%' + @valueToSearch " +
                             "group by C.CustomerID, C.CompanyName";
                             break;
                         case "Middle":
                             sqlQuery = "select C.CustomerID, C.CompanyName, Count(O.OrderID) " +
                             "from Customers C INNER JOIN Orders O on C.CustomerID = O.CustomerID " +
-                            "where C.CompanyName like " + "'%" + valueToSearc + "%' " +
+                            "where C.CompanyName like  '%' + @valueToSearch +  '%' " +
                             "group by C.CustomerID, C.CompanyName";
                             break;
                         default:
@@ -66,11 +73,23 @@ namespace WebForms
 
 
                 }
-                SqlDataAdapter sde = new SqlDataAdapter(sqlQuery, con);
+                // Command ctor with the sql query and connection object
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                //adding parameter into command collection a way against SQL Injection
+                cmd.Parameters.AddWithValue("@valueToSearch", valueToSearch);
+                
+                // bridge between a DataSet and SQL Server by mapping Fill, which changes the data inthe DataSet to match in the Data Source.
+                SqlDataAdapter sde = new SqlDataAdapter(cmd);
+               
+
                 DataSet ds = new DataSet();
                 sde.Fill(ds);
                 DataGrid1.DataSource = ds;
                 DataGrid1.DataBind();
+
+                
+                CompanyName.Focus();
             }
         }
     }
